963653698208ef610999fb6594daf57fb0045c90
Andrew Forrester <atf@Andrews-MacBook-Pro.local>
2023-12-26 15:10:25 -0500

    first commit

---
+++ paul_homepage/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-k^ny*&m&-=%zzt9b1x*k+d@08)d10+xkcv8^ev3_ql7er25!q8"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
