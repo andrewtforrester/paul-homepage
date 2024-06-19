df3f4722e5275da70a80b2362a3ca24f30d9b331
Andrew Forrester <atf@Andrews-MacBook-Pro.local>
2024-06-18 21:38:28 -0400

    start work on new site

--- home/models.py
+++ home/models.py
@@ -6,6 +6,17 @@ from wagtail import blocks
 from wagtail_color_panel.fields import ColorField
 from wagtail_color_panel.edit_handlers import NativeColorPanel
 from wagtail.images.blocks import ImageChooserBlock
+from wagtail.contrib.settings.models import (
+    BaseGenericSetting,
+    BaseSiteSetting,
+    register_setting,
+)
+
+@register_setting
+class GenericSiteSettings(BaseGenericSetting):
+    hero_image = models.ForeignKey(
+        'wagtailimages.Image', on_delete=models.CASCADE, blank=True, null=True, related_name='+'
+    )
 
 
 class HomePage(Page):
@@ -13,41 +24,38 @@ class HomePage(Page):
         'wagtailimages.Image', on_delete=models.CASCADE, blank=True, null=True, related_name='+'
     )
 
-    sidebar_color = ColorField(default="#000000")
-    button_color = ColorField(default="#000000")
+    # sidebar_color = ColorField(default="#000000")
+    # button_color = ColorField(default="#000000")
 
     
 
     page_body = StreamField([
-        ('section', blocks.StructBlock([
-            ('section_title', blocks.CharBlock()),
-            ('section_content', blocks.StreamBlock([
-                ('heading', blocks.CharBlock(form_classname="title")),
-                ('paragraph', blocks.RichTextBlock()),
-                ('numbered_list', blocks.StreamBlock([
-                    ('item', blocks.RichTextBlock()),
-                ])),
-                ('image', ImageChooserBlock()),
-                ('video_embed', blocks.TextBlock()),
-            ]),)
+        ('heading', blocks.CharBlock(form_classname="title")),
+        ('paragraph', blocks.RichTextBlock()),
+        ('numbered_list', blocks.StreamBlock([
+            ('item', blocks.RichTextBlock()),
         ])),
+        ('image', ImageChooserBlock()),
+        ('video_embed', blocks.TextBlock()),
     ], collapsed=True, use_json_field=True)
 
 
-    style_panels = [
-        FieldPanel('profile_image'),
-        NativeColorPanel('sidebar_color'),
-        NativeColorPanel('button_color'),
-    ]
+    # style_panels = [
+    #     FieldPanel('profile_image'),
+    #     NativeColorPanel('sidebar_color'),
+    #     NativeColorPanel('button_color'),
+    # ]
 
     content_panels = [
+        FieldPanel('title'),
         FieldPanel('page_body'),
     ]
 
     edit_handler = TabbedInterface([
         ObjectList(content_panels, heading="Page Content"),
-        ObjectList(style_panels, heading="Page Style"),
+        # ObjectList(style_panels, heading="Page Style"),
         ObjectList(Page.promote_panels, heading='SEO'),
+        ObjectList(Page.settings_panels, heading='Settings'),
     ])
 
     content_panels = Page.content_panels + [
